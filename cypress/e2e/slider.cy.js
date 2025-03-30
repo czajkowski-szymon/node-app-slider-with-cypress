f1a1f4981db2a4340001b72cf7d83b4021aacdef
Szymon <sczajkowski83@gmail.com>
2025-03-30 15:34:50 +0200

    dodatnie testu e2e i dodanie skryptu dla hotjar

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,16 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Verify if decription of every slide is correct', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide-active').should('contain', 'Rome').should('contain', 'Italy')
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'London').should('contain', 'United Kingdom');
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Paris').should('contain', 'France');
+  });
+});
